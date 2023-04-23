d68be2f446b92c8d3a32b702ca5bc0787ef5a717
ana-lapas <anapaulalapas@gmail.com>
2023-04-22 21:30:09 -0400

    Fix: errors hotel

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -29,24 +29,7 @@ async function getAllHotels(userId: number) {
 }
 
 async function getHotelsById(userId: number, hotelId: number) {
-  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-  if (!enrollment) {
-    throw notFoundError();
-  }
-
-  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
-  if (!ticket) {
-    throw notFoundError();
-  }
-  if (ticket.status === 'RESERVED') {
-    throw cannotShowHotelsError();
-  }
-  if (ticket.TicketType.isRemote) {
-    throw cannotShowHotelsError();
-  }
-  if (!ticket.TicketType.includesHotel) {
-    throw cannotShowHotelsError();
-  }
+  await checkHotels(userId);
 
   const hotel = await hotelsRepository.getHotelsRoomId(hotelId);
   if (!hotel || hotel.Rooms.length === 0) {
